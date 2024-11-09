bcc12239ffb4f5f6e5cbd055dbb273094378cc4f
Sukhman Kang <140189696+Sukhman25K@users.noreply.github.com>
2024-11-09 16:18:56 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function fnDarkMode(){
+function fnColourMode(){
     if (document.documentElement.getAttribute('data-bs-theme') == 'dark'){
         document.documentElement.setAttribute('data-bs-theme','light')
         document.getElementById('btnColourMode').classList.remove('btn-light')
@@ -30,18 +30,29 @@ function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark })
       return localStorageTheme;
     }
   
-    if (systemSettingDark.matches) {
-      return "dark";
-    }
-  
-    return "light";
+    return systemSettingDark.matches ? "dark" : "light";
 }
 
 const localStorageTheme = localStorage.getItem("theme");
 const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
 let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
 document.documentElement.setAttribute('data-bs-theme',currentThemeSetting)
-fnDarkMode()
+
+if (currentThemeSetting === 'dark') {
+    document.getElementById('btnColourMode').classList.remove('btn-dark');
+    document.getElementById('btnColourMode').classList.add('btn-light');
+    document.getElementById('imgMoon').src = 'Assets/moon-dark.svg';
+    document.getElementById('imgGithub').src = 'Assets/github-light.svg';
+    document.getElementById('imgLinkedin').src = 'Assets/linkedin-light.svg';
+    document.getElementById('imgEmail').src = 'Assets/envelope-at-light.svg';
+} else {
+    document.getElementById('btnColourMode').classList.remove('btn-light');
+    document.getElementById('btnColourMode').classList.add('btn-dark');
+    document.getElementById('imgMoon').src = 'Assets/moon-light.svg';
+    document.getElementById('imgGithub').src = 'Assets/github-dark.svg';
+    document.getElementById('imgLinkedin').src = 'Assets/linkedin-dark.svg';
+    document.getElementById('imgEmail').src = 'Assets/envelope-at-dark.svg';
+}
 
 window.onscroll = function(){
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
